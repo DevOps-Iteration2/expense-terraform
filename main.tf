f7305353e8669c88ee344db03d1da90ee2a77095
Vivek <vivekanandareddy7677@gmail.com>
2025-02-25 23:04:51 -0600

    Removed mysql in expense terraform

--- main.tf
+++ main.tf
@@ -9,24 +9,13 @@ module "frontend" {
 }
 
 module "backend" {
-  depends_on     = [module.mysql]
+#  depends_on     = [module.mysql]
 
   source         = "./modules/app"
   env            = var.env
   instance_type  = var.instance_type
   zone_id        = var.zone_id
   component      = "backend"
-  ssh_user       = var.ssh_user
-  ssh_pass       = var.ssh_pass
-}
-
-module "mysql" {
-  source         = "./modules/mysql"
-
-  env            = var.env
-  component      = "mysql"
-  instance_type  = var.instance_type
-  zone_id        = var.zone_id
   ssh_user       = var.ssh_user
   ssh_pass       = var.ssh_pass
 }
--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.sg.vpc_id]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags   = {
     Name = var.component
   }
