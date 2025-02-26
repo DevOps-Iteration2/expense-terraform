73c6af6c7546aaf6dd8e0449957732e704d2f84b
Vivek <vivekanandareddy7677@gmail.com>
2025-02-25 20:01:24 -0600

    Setting up observability tools pipeline

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -7,13 +7,16 @@ resource "aws_instance" "instance" {
   }
 }
 resource "null_resource" "ansible" {
+
   provisioner "remote-exec" {
+
     connection {
       type     = "ssh"
       user     = var.ssh_user
       password = var.ssh_pass
       host     = aws_instance.instance.public_ip
     }
+
     inline = [
     "sudo pip3.11 install ansible",
     "ansible-pull -i localhost, -U https://github.com/Vivekananda277/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component}" # Shell commands so utilizing ${var.env}
