3f4b74cbd69f844c98f812a261f524e5ace050b8
Vivek <vivekanandareddy7677@gmail.com>
2025-02-27 18:57:53 -0600

    Setting up node_exporter and prometheus config file

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -2,9 +2,10 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.sg.id]
-  tags   = {
-    Name = var.component
+  tags      = {
+    Name    = var.component
     monitor = "yes"
+    env     = var.env
   }
 }
 resource "null_resource" "ansible" {
