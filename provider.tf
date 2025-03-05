92aece0c072b9d689f612ee80651d0368c0ed3b7
Vivek <vivekanandareddy7677@gmail.com>
2025-03-05 00:31:37 -0600

    vault address update

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address         = "https://54.162.118.192:8200"
+  address         = "https://vault-internal.devopsjourney.fun:8200"
   skip_tls_verify = true
   token           = var.vault_token
 }
