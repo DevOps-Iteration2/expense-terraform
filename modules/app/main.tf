0e1af386054659b2650d615030f231172d303c88
Vivek <vivekanandareddy7677@gmail.com>
2025-03-04 22:38:04 -0600

    Update expense/appmodule connection details

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -18,9 +18,9 @@ resource "null_resource" "ansible" {
     }
 
     inline = [
-    "sudo pip3.11 install ansible",
+    "sudo pip3.11 install ansible hvac",
     "ansible-pull -i localhost, -U https://github.com/DevOps-Iteration2/expense-ansible get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
-    "ansible-pull -i localhost, -U https://github.com/DevOps-Iteration2/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @common.json -e @app.json" # Shell commands so utilizing ${var.env}
+    "ansible-pull -i localhost, -U https://github.com/DevOps-Iteration2/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json" # Shell commands so utilizing ${var.env}
     ]
   }
 }
