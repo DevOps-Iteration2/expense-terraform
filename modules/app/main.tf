beb37a8ffcb35a3257b644355977f6f39234bd9e
Vivek <vivekanandareddy7677@gmail.com>
2025-03-04 23:29:05 -0600

    Update pipelines with vault token

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -20,7 +20,8 @@ resource "null_resource" "ansible" {
     inline = [
     "sudo pip3.11 install ansible hvac",
     "ansible-pull -i localhost, -U https://github.com/DevOps-Iteration2/expense-ansible get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
-    "ansible-pull -i localhost, -U https://github.com/DevOps-Iteration2/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json" # Shell commands so utilizing ${var.env}
+    "ansible-pull -i localhost, -U https://github.com/DevOps-Iteration2/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json",
+    "rm -f ~/secrets.json ~/app.json"
     ]
   }
 }
