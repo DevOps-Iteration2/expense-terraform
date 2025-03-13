bdc84444c9102f3a56f110044613955c88ac42b2
Vivek <vivekanandareddy7677@gmail.com>
2025-03-12 21:28:19 -0500

    Missed count in public route table creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -96,7 +96,7 @@ resource "aws_route_table" "frontend" {
 
   route {
     cidr_block             = "0.0.0.0/0"
-    nat_gateway_id         = aws_nat_gateway.ngw.id
+    nat_gateway_id         = aws_nat_gateway.ngw[count.index].id
   }
 
   tags = {
