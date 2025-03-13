0faa0c17e16fd069bb64d65c743d957665bb1313
Vivek <vivekanandareddy7677@gmail.com>
2025-03-12 19:53:44 -0500

    Setting up elk pipeline

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -67,7 +67,7 @@ resource "aws_nat_gateway" "ngw" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(var.public_subnets)
+  count          = length(var.public_subnets)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public[count.index].id
 }
